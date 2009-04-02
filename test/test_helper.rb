80291ccacea9096b9036473f24273c226b2c42e0
Gabe da Silveira <gabe@websaviour.com>
2009-04-01 20:50:59 -0700

    Initial commit to safe-nested-hash.

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'safe_nested_hash'
+
+class Test::Unit::TestCase
+end
